478efc5177d0bf386db45d6a2f206a305ded9f26
Bảo Bảo <thanh.bao@outlook.com>
2023-04-24 16:04:34 +0700

    add page create new product: add indicator, successful, error

--- baobaostore.com/webapp/controller/ZC_PHONE_PRICE_Create.controller.js
+++ baobaostore.com/webapp/controller/ZC_PHONE_PRICE_Create.controller.js
@@ -3,49 +3,42 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     'sap/m/MessageToast',
     "sap/m/MessageBox",
+    'sap/ui/core/BusyIndicator'
 ],
-    function (Controller, JSONModel, MessageToast, MessageBox) {
+    function (Controller, JSONModel, MessageToast, MessageBox, BusyIndicator) {
         "use strict";
 
         return Controller.extend("baobaostore.com.controller.ZC_PHONE_PRICE_Create", {
 
             onInit: function () {
                 let oNewPhone = new JSONModel({
-                    id: "fdf4534",
-                    specification_id: 2323,
-                    color: "fddfd543534",
-                    storage: 432,
-                    guarantee: 323,
-                    depreciation: 3232,
-                    fake_price: 3232323,
-                    real_price: 32323232332,
-                    status: "dffdf"
-                });
+                    hello: {
+                        id: "fdf4534",
+                        specification_id: 2323,
+                        color: "fddfd543534",
+                        storage: 432,
+                        guarantee: 11,
+                        depreciation: 8,
+                        fake_price: 3232323,
+                        real_price: 32323232,
+                        status: "dffdf"
+                    }
+                })
                 this.getView().setModel(oNewPhone, "new_phone");
             },
 
             submitNewPhone: function () {
-
-                const _newPhone = {
-
-                    id: "fdf",
-                    specification_id: 2323,
-                    color: "fddfd",
-                    storage: 432,
-                    guarantee: 2,
-                    depreciation: 2,
-                    fake_price: 3232323,
-                    real_price: 53434,
-                    status: "dffdf"
-
-                };
+                BusyIndicator.show(0);
+                const _newPhone = this.getView().getModel('new_phone').getProperty("/hello");
 
                 this.getView().getModel().create("/ZC_PHONE_PRICE", _newPhone, {
                     success: function () {
                         MessageToast.show("Create successful");
+                        BusyIndicator.hide();
                     },
                     error: function (err) {
-                        MessageBox.error(JSON.parse(err.responseText).error.message.value)
+                        MessageBox.error(JSON.parse(err.responseText).error.message.value);
+                        BusyIndicator.hide();
                     }
                 });
 
