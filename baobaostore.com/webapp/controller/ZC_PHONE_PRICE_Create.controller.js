e7178d167183eb736fc34d43beb19d9fd5cf5036
Bảo Bảo <thanh.bao@outlook.com>
2023-04-25 10:20:16 +0700

    .

--- baobaostore.com/webapp/controller/ZC_PHONE_PRICE_Create.controller.js
+++ baobaostore.com/webapp/controller/ZC_PHONE_PRICE_Create.controller.js
@@ -3,35 +3,103 @@ sap.ui.define([
     "sap/ui/model/json/JSONModel",
     'sap/m/MessageToast',
     "sap/m/MessageBox",
-    'sap/ui/core/BusyIndicator'
+    'sap/ui/core/BusyIndicator',
+    "sap/ui/model/Filter",
 ],
-    function (Controller, JSONModel, MessageToast, MessageBox, BusyIndicator) {
+    function (Controller, JSONModel, MessageToast, MessageBox, BusyIndicator, Filter) {
         "use strict";
 
         return Controller.extend("baobaostore.com.controller.ZC_PHONE_PRICE_Create", {
 
             onInit: function () {
                 let oNewPhone = new JSONModel({
-                    hello: {
-                        id: "fdf4534",
-                        specification_id: 2323,
-                        color: "fddfd543534",
-                        storage: 432,
-                        guarantee: 11,
-                        depreciation: 8,
-                        fake_price: 3232323,
-                        real_price: 32323232,
-                        status: "dffdf"
+                    value: {
+                        id: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        specification_id: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        color: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        storage: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        guarantee: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        depreciation: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        fake_price: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        real_price: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        },
+                        status: {
+                            value: null,
+                            valueState: null,
+                            valueStateText: null
+                        }
                     }
+
                 })
                 this.getView().setModel(oNewPhone, "new_phone");
+
+
+            },
+
+            checkPhoneId: function (e) {
+                const _phoneId = e.getParameters().value;
+                let _oModel = this.getView().getModel('new_phone');
+                this.getView().getModel().read('/ZC_PHONE_PRICE/$count', {
+                    filters: [new Filter("id", "EQ", _phoneId)],
+                    success: function (count) {
+                        console.log("Odata", count)
+                        if (count > 0) {
+                            _oModel.setProperty("/value/id/valueStateText", "phone ID already exists!")
+                        } else {
+
+                        }
+                    },
+                    error: function (err) {
+                        console.log("ERR", err)
+                    }
+                })
+
             },
 
             submitNewPhone: function () {
                 BusyIndicator.show(0);
-                const _newPhone = this.getView().getModel('new_phone').getProperty("/hello");
+                const _newPhoneFromView = this.getView().getModel('new_phone').getProperty("/value");
 
-                this.getView().getModel().create("/ZC_PHONE_PRICE", _newPhone, {
+                const _newPhonePayloadOdata = {};
+
+                for (const property in _newPhoneFromView) {
+                    _newPhonePayloadOdata[property] = _newPhoneFromView[property].value;
+                }
+
+                console.log("XXXX", _newPhonePayloadOdata)
+
+                this.getView().getModel().create("/ZC_PHONE_PRICE", _newPhonePayloadOdata, {
                     success: function () {
                         MessageToast.show("Create successful");
                         BusyIndicator.hide();
