5b9e2295873e1af6c9974f4f334b7cebfca1a27c
Bảo Bảo <thanh.bao@outlook.com>
2023-05-04 15:04:23 +0700

    Week2 Unit 4: NotFound

---
+++ movies/webapp/controller/NotFound.controller.js
@@ -0,0 +1,43 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History",
+    "sap/ui/core/UIComponent"
+], function (Controller, History, UIComponent) {
+    "use strict";
+
+
+    return Controller.extend("movies.controller.NotFound", {
+
+        onInit: function () {
+            var oRouter, oTarget;
+
+            oRouter = UIComponent.getRouterFor(this);
+            oTarget = oRouter.getTarget("notFound");
+            oTarget.attachDisplay(function (oEvent) {
+                this._oData = oEvent.getParameter("data");	// store the data
+            }, this);
+        },
+
+        // override the parent's onNavBack (inherited from BaseController)
+        onNavBack: function () {
+            // in some cases we could display a certain target when the back button is pressed
+            if (this._oData && this._oData.fromTarget) {
+                UIComponent.getRouterFor(this).getTargets().display(this._oData.fromTarget);
+                delete this._oData.fromTarget;
+                return;
+            }
+
+            var oHistory, sPreviousHash;
+
+            oHistory = History.getInstance();
+            sPreviousHash = oHistory.getPreviousHash();
+
+            if (sPreviousHash !== undefined) {
+                window.history.go(-1);
+            } else {
+                UIComponent.getRouterFor(this).navTo("RouteDashboard", {}, true /*no history*/);
+            }
+        }
+    });
+});
+
